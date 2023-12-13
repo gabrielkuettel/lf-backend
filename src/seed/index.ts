432f8e3dfe6a15f7c48a1cd17ced3fda4cd11757
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-12-12 21:41:42 -0700

    feat: change seed account credentials

--- src/seed/index.ts
+++ src/seed/index.ts
@@ -4,8 +4,8 @@ export const seed = async (payload: Payload): Promise<void> => {
   await payload.create({
     collection: 'users',
     data: {
-      email: 'demo@demo.com',
-      password: 'demo',
+      email: 'admin@admin.com',
+      password: 'admin',
       roles: ['admin'],
     },
   })
